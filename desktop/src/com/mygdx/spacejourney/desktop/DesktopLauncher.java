65b19a4d97a3dee0f55bed51ac5ba2c5b435769b
Nguyen Quang Duc <nqduc74@gmail.com>
2021-11-04 08:31:21 +0700

    init project

---
+++ desktop/src/com/mygdx/spacejourney/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.spacejourney.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.spacejourney.SpaceJourney;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceJourney(), config);
+	}
+}
