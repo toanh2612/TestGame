cac469c9227277d06db07cc92c20a67101592eec
Nguyen Quang Duc <nqduc74@gmail.com>
2021-11-04 13:11:30 +0700

    update: setup directory and code for scrolling background

---
+++ core/src/utility/Constant.java
@@ -0,0 +1,8 @@
+package utility;
+
+import com.badlogic.gdx.Gdx;
+
+public class Constant {
+    public static final int WIDTH = Gdx.graphics.getWidth();
+    public static final int HEIGHT = Gdx.graphics.getHeight();
+}
